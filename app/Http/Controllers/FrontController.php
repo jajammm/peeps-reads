0ec2978cf90b6c9a68a42bdbd95db4b2ae1000f3
jajammm <nasiliwet259@gmail.com>
2024-12-28 11:38:23 +0700

    Show category and banners

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ArticleNews;
+use App\Models\Author;
+use App\Models\BannerAds;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index()
     {
-        return view('front.index');
+        $categories = Category::all();
+
+        $articles = ArticleNews::with('category')
+        ->where('is_featured', 'not_featured')
+        ->latest()
+        ->take(3)
+        ->get();
+    
+        $featured_articles = ArticleNews::with('category')
+        ->where('is_featured', 'featured')
+        ->inRandomOrder()
+        ->take(3)
+        ->get();
+
+        $authors = Author::all();
+
+        $bannerads = BannerAds::where('is_active', 'active')
+        ->where('type', 'banner')
+        ->inRandomOrder()
+        // ->take(1)
+        ->first();
+
+        return view('front.index', compact('categories', 'articles', 'authors', 'featured_articles', 'bannerads'));
     }
 }
